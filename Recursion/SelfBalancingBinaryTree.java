cf127ff9ab91152ad9d9b6a2343b2cc116a935f0
❖Mʀ᭄Anshᴿᵒˢʰᵃⁿ࿐ <ianshroshan@gmail.com>
2024-04-23 22:08:20 +0530

    studied BST

---
+++ Recursion/SelfBalancingBinaryTree.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class SelfBalancingBinaryTree {
+
+}
+
+// adelson veleski lendon
+class AVLTree {
+
+    private Node root;
+
+    private class Node {
+
+        private int data;
+        private Node left;
+        private Node right;
+    }
+}
